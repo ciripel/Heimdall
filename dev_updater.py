76df8403952b215709dad0a10b7f0174511f4c64
ciripel <nedelcu.alexandru@yahoo.com>
2020-05-07 23:20:41 +0300

    defined repo writer

--- dev_updater.py
+++ dev_updater.py
@@ -20,6 +20,8 @@ COMMIT_MESSAGE = "Last work at " + complete_list[-1]["created_at"][:-7]
 def git_push():
     try:
         repo = Repo(PATH_OF_GIT_REPO)
+        repo.config_writer().set_value("user", "name", "ciripel").release()
+        repo.config_writer().set_value("user", "email", "nedelcu.alexandru@yahoo.com").release()
         repo.git.add(update=True)
         repo.git.commit(m=COMMIT_MESSAGE)
         origin = repo.remote(name="origin")
